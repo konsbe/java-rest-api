e6077d8a37572cfae5b14bbb7b97ef5ad13509c7
k.berdelis <k.berdelis@deepsea.ai>
2023-02-08 00:19:31 +0200

    cleaning problems

--- src/main/java/com/vone/javarest/entity/CommentEntity.java
+++ src/main/java/com/vone/javarest/entity/CommentEntity.java
@@ -3,13 +3,10 @@ package com.vone.javarest.entity;
 import java.sql.Timestamp;
 import java.util.Random;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
